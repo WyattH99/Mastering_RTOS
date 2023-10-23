784cec846a9999b2a01a560369a2f9e3e0b67466
Wyatt-Desktop <WyattHansen99@gmail.com>
2023-10-23 11:30:59 -0500

    Section 5 Complete FreeRTOS Project created and built

---
+++ Workspace/RTOS_workspace/FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h:
