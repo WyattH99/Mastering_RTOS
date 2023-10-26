47efadabf4b7867a1d2960bb6b07348380c4d1ad
Wyatt-Desktop <WyattHansen99@gmail.com>
2023-10-26 13:39:10 -0500

    Completed Exercise 005

---
+++ Workspace/RTOS_workspace/005_LED_Task_NotifyAndDeletion/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ /home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+/home/wyatt/Desktop/Mastering_RTOS/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
